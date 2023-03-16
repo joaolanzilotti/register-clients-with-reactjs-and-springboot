4eb6dbe5cad1f2eaf287e24a3025040f5b2ee988
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-03-16 14:30:45 -0300

    project api-cliente initialized

---
+++ src/main/java/com/corporation/apiclient/ApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.corporation.apiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/corporation/apiclient/ApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corporation.apiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
