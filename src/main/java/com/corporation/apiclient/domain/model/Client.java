cf7e134a0093f3af2a50e4122430ac18e56c83ff
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-03-17 15:03:35 -0300

    initialized FlyWay in Project

--- src/main/java/com/corporation/apiclient/domain/model/Client.java
+++ src/main/java/com/corporation/apiclient/domain/model/Client.java
@@ -1,16 +1,22 @@
 package com.corporation.apiclient.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Client {
+@Entity
+public class Client implements Serializable {
 
+    @Id
     private Long id;
     private String name;
     private String email;
---
+++ src/main/resources/db/migration/V001__cria-tabela-cliente.sql
@@ -0,0 +1,7 @@
+create table client(
+    id bigint not null auto_increment,
+    nome varchar(60) not null,
+    email varchar(60) not null ,
+    telefone varchar(60) not null,
+    primary key (id)
+);
