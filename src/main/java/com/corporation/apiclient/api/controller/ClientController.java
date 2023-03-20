66a43b7b5eb41a8291e9cecd8ac454313d71ab4c
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-03-20 14:34:08 -0300

    created methods DeleteClient , AddClient and UpdateCliente

--- src/main/java/com/corporation/apiclient/api/controller/ClientController.java
+++ src/main/java/com/corporation/apiclient/api/controller/ClientController.java
@@ -4,12 +4,12 @@ import com.corporation.apiclient.domain.model.Client;
 import com.corporation.apiclient.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.ArrayList;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/clients")
 @RestController
@@ -19,9 +19,51 @@ public class ClientController {
     private ClientService clientService;
 
     @GetMapping
-    public ResponseEntity<List<Client>> listClients(){
+    public ResponseEntity<List<Client>> listClients() {
         List<Client> listClient = clientService.listClient();
         return ResponseEntity.ok().body(listClient);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<Client>> findClientById(@PathVariable Long id) {
+
+        Optional<Client> cliente = clientService.findClientById(id);
+
+        if (cliente.isPresent()) {
+
+            return ResponseEntity.ok().body(cliente);
+
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Client> addClient(@RequestBody Client client) {
+        Client cliente = clientService.addCliente(client);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cliente.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Client> updateClient(@PathVariable Long id, @RequestBody Client client){
+
+        if(!clientService.existByClientId(id)){
+            return ResponseEntity.notFound().build();
+        }
+        client.setId(id);
+        client = clientService.addCliente(client);
+        return ResponseEntity.ok().body(client);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable Long id){
+        if(!clientService.existByClientId(id)){
+            return ResponseEntity.notFound().build();
+        }
+        clientService.deleteClient(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/corporation/apiclient/services/ClientService.java
+++ src/main/java/com/corporation/apiclient/services/ClientService.java
@@ -4,9 +4,11 @@ import com.corporation.apiclient.domain.model.Client;
 import com.corporation.apiclient.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService implements Serializable {
@@ -16,6 +18,27 @@ public class ClientService implements Serializable {
 
     public List<Client> listClient(){
         return repository.findAll();
+    }
+
+    public Optional<Client> findClientById(Long id){
+        Optional<Client> cliente = repository.findById(id);
+        return cliente;
+    }
+
+    public Client addCliente(Client client){
+    return repository.save(client);
+    }
+
+    public Client updateclient(Client client){
+        return repository.save(client);
+    }
+
+    public void deleteClient(Long id){
+        repository.deleteById(id);
+    }
+
+    public Boolean existByClientId(Long id){
+        return repository.existsById(id);
     }
 
 }
