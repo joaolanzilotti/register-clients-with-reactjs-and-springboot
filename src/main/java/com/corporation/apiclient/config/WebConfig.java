700aae5e2b4d69083b7a0b80a2d6d78bbeea328c
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-04-11 09:45:37 -0300

    Depedency Jackson-xml Added

---
+++ src/main/java/com/corporation/apiclient/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.corporation.apiclient.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        WebMvcConfigurer.super.configureContentNegotiation(configurer);
+    }
+}
