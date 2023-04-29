878a760c7b0cb287548b05ae44f733758c387ddc
joaolanzilotti <algamerjoao1@hotmail.com>
2023-04-29 12:38:15 -0300

    Create JwtTokenProvider in Progress

---
+++ src/main/java/com/corporation/apiclient/securityjwt/JwtTokenProvider.java
@@ -0,0 +1,53 @@
+package com.corporation.apiclient.securityjwt;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import com.corporation.apiclient.dto.security.TokenDTO;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class JwtTokenProvider {
+
+    @Value("${security.jwt.token.secret-key:secret}")
+    private String secretKey = "secret";
+
+    @Value("${security.jwt.token.expire-lenght:3600000}")
+    private long validityInMilliseconds = 3600000;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    Algorithm algorithm = null;
+
+    @PostConstruct //Iniciar uma acao logo apos a inicializacao do Spring
+    protected void init(){
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        algorithm = Algorithm.HMAC256(secretKey.getBytes());
+    }
+
+    public TokenDTO createAcessToken(String username, List<String> roles){
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+        var accessToken = getAccessToken(username, roles, now, validity);
+        var refreshToken = getRefreshToken(username, roles, now);
+        return new TokenDTO(username, true, now, validity, accessToken, refreshToken);
+    }
+
+    private String getRefreshToken(String username, List<String> roles, Date now) {
+        return null;
+    }
+
+    private String getAccessToken(String username, List<String> roles, Date now, Date validity) {
+        return null;
+    }
+
+}
