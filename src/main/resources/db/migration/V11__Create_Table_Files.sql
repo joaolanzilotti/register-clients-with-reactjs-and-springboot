540564a459ed41ff30dc1086ad169aa009c9f0b2
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-05-09 10:41:41 -0300

    Changed Query Create Table Files

--- src/main/resources/db/migration/V11__Create_Table_Files.sql
+++ src/main/resources/db/migration/V11__Create_Table_Files.sql
@@ -1,8 +1,8 @@
-﻿CREATE TABLE files (
-                       id BIGINT NOT NULL AUTO_INCREMENT,
-                       name VARCHAR(255),
-                       content_type VARCHAR(255),
-                       size BIGINT,
-                       data LONGBLOB,
-                       PRIMARY KEY (id)
+﻿CREATE TABLE `files` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255),
+    `content_type` VARCHAR(255),
+    `size` BIGINT,
+    `data` LONGBLOB,
+    PRIMARY KEY (id)
 );
