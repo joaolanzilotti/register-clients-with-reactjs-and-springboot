7f732ba1b6394556f61b56d5b3a9d4c1c356bbba
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-05-17 13:38:17 -0300

    Implemented useState in Component Header

--- Frontend/front-client/src/App.js
+++ Frontend/front-client/src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './Header';
 
 function App() {
+
+    //So consigo mudar valores se usar os estados com useStates
+    //Array {Valor, changeValueFunction}
+  const [counter, setCounter] = useState(0);
+
+
+  function increment() {
+   setCounter(counter + 1);
+  }
+
   return(
     //JSX JavaScript XML
+    <div>
     <Header>
-      Client API Client
+     
+      Counter: {counter}
+      
     </Header>
+    <button onClick={increment}>Add</button>
+    </div>
 
   ) ;
 }
