296d839f240c0875fcfeeff57a9764520c3dd9e4
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-05-17 14:33:38 -0300

    Added Logo and image LoginLock in Client Index

--- Frontend/front-client/src/pages/Login/index.js
+++ Frontend/front-client/src/pages/Login/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import './styles.css'
+import './styles.css';
+import logoImage from '../../assets/logo.png'
+import loginLock from '../../assets/login.png'
 
 //Propriedade com React - Definindo que o metodo vai receber Filhos (Children) e especificando dentro do H1 também
 function Login() {
 return (
 
-        <h1>Hello</h1>
+        <div className="login-container">
+
+                <section className="form">
+                <img src={logoImage} alt="Client Logo" />
+                <form>
+                        <h1>Acess your Account</h1>
+                        <input type="text" placeholder="Username"/>
+                        <input type="password" placeholder="Password"/>
+                        <button type="submit"> Login</button>
+                </form>
+                        
+                </section>
+                
+                <img src={loginLock} alt="Login" />
+        </div>
         )
 
 };
