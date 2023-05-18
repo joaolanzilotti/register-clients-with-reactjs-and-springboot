8cd75e421e94eb3d9d2761e80b6d76a581ac4f3d
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-05-18 11:58:52 -0300

    Added new Import React-icons and Added Changes for Page Client

--- Frontend/front-client/src/pages/Client/index.js
+++ Frontend/front-client/src/pages/Client/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import { FiPower } from 'react-icons/fi';
+
 import './styles.css';
+
+import logoJP from '../../assets/logoJP.png';
 
 function Client(){
     return(
-        <h1>Client</h1>
+        <div className="client-container">
+            <header>
+                <img src={logoJP} alt="JP"/>
+                <span>Welcome, <strong>João</strong></span>
+                <Link className="button" to="/client/new">Add new Client </Link>
+                <button type="button">
+                    <FiPower size={18} color="#251FC5"/>
+                </button>
+            </header>
+        </div>
     );
 }
 export default Client;
