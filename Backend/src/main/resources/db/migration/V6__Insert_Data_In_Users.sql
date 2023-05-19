eaed7215bc4a526e7cca32bb1717faba8ab830b3
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-05-19 14:49:14 -0300

    Changed Password the DbMigration V6

--- Backend/src/main/resources/db/migration/V6__Insert_Data_In_Users.sql
+++ Backend/src/main/resources/db/migration/V6__Insert_Data_In_Users.sql
@@ -1,3 +1,3 @@
 ﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
-																																				  ('joaolanzilotti', 'João Pedro Lanzilotti Ferretti', '1e3cdeeaaaeeda173ff6d002e7cb5e3f91ebc354dcff52156c9eaba1793a3a5e5bee306c11099e22', b'1', b'1', b'1', b'1'),
-																																				  ('flavio', 'Flavio Costa', '362ad02420268beeb22d3a1f0d92749df461d7f4b74c9433d7415bdeef1b2902f4eb1edaecb37cb3', b'1', b'1', b'1', b'1');
+																																				  ('joaolanzilotti', 'João Pedro Lanzilotti Ferretti', '$2a$12$fVB6CVvVPD7rbaH06DrBlO6Dothjk95NUkI2mhQdi4Q6aTl2r9oW2', b'1', b'1', b'1', b'1'),
+																																				  ('flavio', 'Flavio Costa', '$12$rkblaMgqlhjGgk9r0mx.3Oodkt/k8686KFeciFq24PybFrcF/EnjO', b'1', b'1', b'1', b'1');
