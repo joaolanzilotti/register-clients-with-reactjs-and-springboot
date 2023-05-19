623c92720eeb0815619501bd48f3ebe2193740cb
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-05-19 10:50:42 -0300

    Implemented Style for Page new Client

--- Frontend/front-client/src/pages/NewClient/index.js
+++ Frontend/front-client/src/pages/NewClient/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 
 import './styles.css';
-import logoJP from '../../assets/logoJP.png';
+import logoJP from '../../assets/newUser.png';
 import {Link} from "react-router-dom";
-import { FiArrowLeft } from "react-icons/fi";
+import {FiArrowLeft, FiUserPlus} from "react-icons/fi";
 
 export default function NewClient(){
     return(
         <div className="new-client-container">
             <div className="content">
-                <section>
+                <section className="form">
                     <img src={logoJP} alt="JP"/>
-                    <h1>Add New CLient</h1>
+                    <h1>Add New Client</h1>
                     <p>Enter the client information and click on 'Add'!</p>
                     <Link className="back-link" to="/clients">
-                        <FiArrowLeft size={16} color="#251FC5"/>
-                        Home
+                        <div className="container-button">
+                            <div className="iconArrowLeft"><FiArrowLeft size={16} color="blue"/></div>
+                            <div className="textButton">Home</div>
+                        </div>
                     </Link>
+                </section>
                     <form>
                         <input placeholder="Name"/>
                         <input placeholder="E-mail"/>
@@ -25,9 +28,8 @@ export default function NewClient(){
                         <input placeholder="CPF"/>
                         <input type="date"/>
                         <input placeholder="Cellphone"/>
-                        <button className="buttonNewClient" type="submit">Add</button>
+                        <button className="button" type="submit">Add</button>
                     </form>
-                </section>
             </div>
         </div>
     );
