b35dbd27334bce925dc15a84048442e232d2bf71
joaolanzilotti <algamerjoao1@hotmail.com>
2023-06-05 17:30:28 -0300

    Implemented API ViaCEP and IBGE UFs in Page New Adress

--- Frontend/front-client/src/pages/NewAdress/index.js
+++ Frontend/front-client/src/pages/NewAdress/index.js
@@ -1,51 +1,49 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
 import loadingGif from '../../assets/loadingTwoWhite.gif';
 import homeadress from '../../assets/homeadress.png';
-import {Link, useNavigate, useParams} from "react-router-dom";
-import {FiArrowLeft, FiUserPlus} from "react-icons/fi";
-import {ToastContainer, toast} from "react-toast";
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { FiArrowLeft, FiUserPlus } from 'react-icons/fi';
+import { ToastContainer, toast } from 'react-toast';
+import InputMask from 'react-input-mask';
 
 import api from '../../services/api';
 
 export default function NewAddress() {
-
     const [id, setId] = useState(null);
     const [street, setStreet] = useState('');
     const [district, setDistrict] = useState('');
     const [number, setNumber] = useState('');
     const [city, setCity] = useState('');
     const [state, setState] = useState('');
+    const [cep, setCep] = useState('');
     const [ufs, setUfs] = useState([]);
 
     const accessToken = localStorage.getItem('accessToken');
 
     const [showLoading, setShowLoading] = useState(false);
 
-    const {userId} = useParams();
-    const {adressId} = useParams();
+    const { userId } = useParams();
+    const { adressId } = useParams();
 
-    //Funcao navigate para Enviar a Rota
     const navigate = useNavigate();
 
-
-    //async function é uma funcao que aguarda o carregamento da pagina.
     async function loadAdress() {
         try {
             const response = await api.get(`/api/adress/${adressId}`, {
                 headers: {
-                    Authorization: `Bearer ${accessToken}`
-                }
-            })
-            setId(response.data.id)
-            setStreet(response.data.street)
-            setDistrict(response.data.district)
-            setNumber(response.data.number)
-            setCity(response.data.city)
-            setState(response.data.state)
-
-        } catch (erro) {
-            toast.error('Error recovering address!, Try again!')
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            });
+            setId(response.data.id);
+            setStreet(response.data.street);
+            setDistrict(response.data.district);
+            setNumber(response.data.number);
+            setCity(response.data.city);
+            setCep(response.data.cep);
+            setState(response.data.state);
+        } catch (error) {
+            toast.error('Error recovering address! Try again!');
             navigate('/users');
         }
     }
@@ -55,125 +53,148 @@ export default function NewAddress() {
             params: {
                 orderBy: 'nome',
             },
-        })
+        });
 
         setUfs([...ufs, ...response.data]);
     }
 
     useEffect(() => {
-        fetchUfs().then();
-
-    }, [])
-
+        fetchUfs();
+    }, []);
 
     useEffect(() => {
         if (adressId === '0') return;
         else {
-            loadAdress().then();
+            loadAdress();
         }
+    }, [adressId]);
 
-
-    }, [adressId])
-
-    //funcao para nao fazer a pagina dar Refresh
     async function SaveOrUpdateAdress(e) {
         e.preventDefault();
         setShowLoading(true);
 
+        if (street.trim() === '' || district.trim() === '' || number.trim() === '' || city.trim() === '' || state.trim() === '') {
+            toast.error('Please fill in all the required fields!');
+            setShowLoading(false);
+            return;
+        }
 
         const data = {
+            cep,
             street,
             district,
             number,
             city,
             state,
-
-        }
+        };
 
         try {
             if (adressId === '0') {
                 const response = await api.post('/api/adress', data, {
-                    //Adicionando na resposta o Header com o Token
                     headers: {
-                        Authorization: `Bearer ${accessToken}`
-                    }
+                        Authorization: `Bearer ${accessToken}`,
+                    },
                 });
 
                 await api.post(`/api/users/userWithAdress/${userId}/${response.data.id}`, null, {
-                    //Adicionando na resposta o Header com o Token
                     headers: {
-                        Authorization: `Bearer ${accessToken}`
-                    }
+                        Authorization: `Bearer ${accessToken}`,
+                    },
                 });
-                toast.success('Address added with Sucess!');
-
+                toast.success('Address added with success!');
             } else {
-                data.id = id
+                data.id = id;
                 await api.put(`/api/adress/${id}`, data, {
-                    //Adicionando na resposta o Header com o Token
                     headers: {
-                        Authorization: `Bearer ${accessToken}`
-                    }
+                        Authorization: `Bearer ${accessToken}`,
+                    },
                 });
-                toast.success('Address Updated with Success!');
+                toast.success('Address updated with success!');
             }
-
-        } catch (err) {
-            toast.error('Error while Record Address! Try Again!');
+        } catch (error) {
+            toast.error('Error while recording address! Try again!');
         } finally {
             setShowLoading(false);
         }
-
     }
+
+    async function fetchAddressData() {
+        try {
+            const response = await api.get(`https://viacep.com.br/ws/${cep}/json/`);
+
+            if (response.data.erro) {
+                toast.error('Invalid CEP!');
+            } else {
+                setStreet(response.data.logradouro);
+                setDistrict(response.data.bairro);
+                setCity(response.data.localidade);
+                setState(response.data.uf);
+            }
+        } catch (error) {
+            toast.error('Error searching CEP data!');
+        }
+    }
+
+    useEffect(() => {
+        if (cep.length === 8) {
+            fetchAddressData().then();
+        }
+    }, [cep]);
 
     return (
         <div className="notification">
-            <ToastContainer position="top-center" delay="3000"/>
+            <ToastContainer position="top-center" autoClose={3000} />
 
             <div className="new-adress-container">
-
                 <div className="content">
                     <section className="form">
-                        <img src={homeadress} alt="JP"/>
+                        <img src={homeadress} alt="JP" />
                         <h1>{adressId === '0' ? 'Add New' : 'Update'} Address</h1>
                         <p>Enter the address information and click on {adressId === '0' ? 'Add' : 'Update'}</p>
                         <Link className="back-link" to="/users">
                             <div className="container-button">
-                                <div className="iconArrowLeft"><FiArrowLeft size={16} color="blue"/></div>
+                                <div className="iconArrowLeft">
+                                    <FiArrowLeft size={16} color="blue" />
+                                </div>
                                 <div className="textButton">Back to Users</div>
                             </div>
                         </Link>
                     </section>
                     <form onSubmit={SaveOrUpdateAdress}>
-                        <label>Street</label>
-                        <input id="street" placeholder="Street" value={street}
-                               onChange={e => setStreet(e.target.value)}/>
-                        <label>District</label>
-                        <input type="text" placeholder="District" value={district}
-                               onChange={e => setDistrict(e.target.value)}/>
-                        <label>Number</label>
-                        <input type="text" placeholder="Number" value={number}
-                               onChange={e => setNumber(e.target.value)}/>
-                        <label>City</label>
-                        <input placeholder="City" value={city} onChange={e => setCity(e.target.value)}/>
-                        <label>State</label>
-                        <select onChange={e => setState(() => e.target.value)} value={state} className="select">
+                        <label>CEP*</label>
+                        <InputMask
+                            id="cep"
+                            placeholder="CEP"
+                            mask="99999-999"
+                            value={cep}
+                            onChange={(e) => setCep(e.target.value.replace('_', '').replace('-', ''))}
+                        />
+                        <label>Street*</label>
+                        <input id="street" placeholder="Street" value={street} onChange={(e) => setStreet(e.target.value)} />
+                        <label>District*</label>
+                        <InputMask type="text" mask="99999" placeholder="District" value={district} onChange={(e) => setDistrict(e.target.value)} />
+                        <label>Number*</label>
+                        <input type="text" placeholder="Number" value={number} onChange={(e) => setNumber(e.target.value)} />
+                        <label>City*</label>
+                        <input placeholder="City" value={city} onChange={(e) => setCity(e.target.value)} />
+                        <label>State*</label>
+                        <select onChange={(e) => setState(e.target.value)} value={state} className="select">
                             {ufs.map((uf) => (
-                                <option key={uf.sigla} value={uf.sigla}>{uf.nome}</option>
+                                <option key={uf.sigla} value={uf.sigla}>
+                                    {uf.nome}
+                                </option>
                             ))}
                         </select>
-                        {/*<input placeholder="State" value={state} onChange={e => setState(e.target.value)}/>*/}
                         <button className="button" type="submit">
                             {showLoading ? (
-                                <img className="loadingGif" src={loadingGif} alt="Spinner"/>
+                                <img className="loadingGif" src={loadingGif} alt="Spinner" />
                             ) : (
                                 adressId === '0' ? 'Add' : 'Update'
                             )}
                         </button>
-
                     </form>
                 </div>
             </div>
         </div>
-    )
+    );
 }
--- Frontend/front-client/src/pages/NewUser/index.js
+++ Frontend/front-client/src/pages/NewUser/index.js
@@ -20,6 +20,7 @@ export default function NewUser() {
     const [cpf, setCpf] = useState('');
     const [birthDay, setBirthDay] = useState('');
     const [cellphone, setCellphone] = useState('');
+    const [sexo, setSexo] = useState('');
     const [adress, setAdress] = useState('');
 
     const emailLogged = localStorage.getItem('email');
@@ -48,7 +49,9 @@ export default function NewUser() {
             setCpf(response.data.cpf)
             setCellphone(response.data.cellphone)
             setBirthDay(response.data.birthDay)
+            setSexo(response.data.sexo)
             setAdress(response.data.adress)
+
         } catch (erro) {
             toast.error('Error recovering User!, Try again!')
             navigate('/users');
@@ -68,112 +71,135 @@ export default function NewUser() {
     async function SaveOrUpdateUser(e) {
         e.preventDefault();
         setShowLoading(true);
-
-
-        const data = {
-            name,
-            email,
-            password,
-            rg,
-            cpf,
-            birthDay,
-            cellphone,
-
-        }
-
-        try {
-            if (userId === '0') {
-                const response = await api.post('/api/users', data, {
-                    //Adicionando na resposta o Header com o Token
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`
-                    }
-                });
-                toast.success('User added with Sucess!');
-                navigate(`/user/newadress/${response.data.id}/0`)
-            } else {
-                data.id = id
-                const response = await api.put(`/api/users/${id}`, data, {
-                    //Adicionando na resposta o Header com o Token
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`
-                    }
-                });
-                if (response.data.adress === null) {
-                    navigate(`/user/newadress/${response.data.id}/0`)
-                }
-                navigate(`/user/newadress/${response.data.id}/${response.data.adress.id}`)
-                toast.success('User Updated with Success!');
-            }
-
-        } catch (err) {
-            if (err.response) {
-                if (err.response.data.message === "Validation failed for classes [com.corporation.apiclient.entities.User] during persist time for groups [jakarta.validation.groups.Default, ]\nList of constraint violations:[\n\tConstraintViolationImpl{interpolatedMessage='Invalid CPF', propertyPath=cpf, rootBeanClass=class com.corporation.apiclient.entities.User, messageTemplate='Invalid CPF'}\n]" ){
-                    toast.error('Invalid CPF');
-                }
-                if(err.response.data.message){
-                    toast.error(err.response.data.message);
-                }
-
-            } else if (err.request) {
-                toast.error('Request Failed. Verify your Connection.');
-            } else {
-                toast.error('An unexpected error occurred. Please, Try Again Later.');
-            }
-        } finally {
+        if (name.trim() === '' || email.trim() === '' || password.trim() === '' || rg.trim() === '' || cpf.trim() === ''|| birthDay.trim() === '' || cellphone.trim() === '' || sexo.trim() === '') {
+            toast.error('Please fill in all the required fields!');
             setShowLoading(false);
+            return;
         }
 
-    }
+
+
+            const data = {
+                name,
+                email,
+                password,
+                rg,
+                cpf,
+                birthDay,
+                cellphone,
+                sexo,
+
+            }
+
+            try {
+                if (userId === '0') {
+                    const response = await api.post('/api/users', data, {
+                        //Adicionando na resposta o Header com o Token
+                        headers: {
+                            Authorization: `Bearer ${accessToken}`
+                        }
+                    });
+                    if (sexo === 'Select' || sexo === '') {
+                        toast.warn('Please, Select a option Valid.');
+                    }else {
+                        toast.success('User added with Sucess!');
+                        navigate(`/user/newadress/${response.data.id}/0`)
+                    }
+                } else {
+                    data.id = id
+                    const response = await api.put(`/api/users/${id}`, data, {
+                        //Adicionando na resposta o Header com o Token
+                        headers: {
+                            Authorization: `Bearer ${accessToken}`
+                        }
+                    });
+                    if (sexo === 'Select' || sexo === '') {
+                        toast.warn('Please, Select a option Valid.');
+                    }else {
+                        if (response.data.adress === null) {
+                            navigate(`/user/newadress/${response.data.id}/0`)
+                        }
+                        navigate(`/user/newadress/${response.data.id}/${response.data.adress.id}`)
+                        toast.success('User Updated with Success!');
+                    }
+                }
+
+            } catch (err) {
+                if (err.response) {
+                    if (err.response.data.message === "Validation failed for classes [com.corporation.apiclient.entities.User] during persist time for groups [jakarta.validation.groups.Default, ]\nList of constraint violations:[\n\tConstraintViolationImpl{interpolatedMessage='Invalid CPF', propertyPath=cpf, rootBeanClass=class com.corporation.apiclient.entities.User, messageTemplate='Invalid CPF'}\n]") {
+                        toast.error('Invalid CPF');
+                    }
+                    if (err.response.data.message) {
+                        toast.error(err.response.data.message);
+                    }
+
+                } else if (err.request) {
+                    toast.error('Request Failed. Verify your Connection.');
+                } else {
+                    toast.error('An unexpected error occurred. Please, Try Again Later.');
+                }
+            } finally {
+                setShowLoading(false);
+            }
+
+        }
+
 
     return (
         <div className="container">
             <ToastContainer position="top-center" delay="3000"/>
-        <div className="new-user-container">
+            <div className="new-user-container">
 
-            <div className="content">
-                <section className="form">
-                    <img src={logoJP} alt="JP"/>
-                    <h1>{userId === '0' ? 'Add New' : 'Update'} User</h1>
-                    <p>Enter the user information and click on {userId === '0' ? 'Next' : 'Next'}</p>
-                    <Link className="back-link" to="/users">
-                        <div className="container-button">
-                            <div className="iconArrowLeft"><FiArrowLeft size={16} color="blue"/></div>
-                            <div className="textButton">Back to Users</div>
-                        </div>
-                    </Link>
-                </section>
-                <form onSubmit={SaveOrUpdateUser}>
-                    <label>Name</label>
-                    <input id="name" placeholder="Name" value={name} onChange={e => setName(e.target.value)}/>
-                    <label>Email</label>
-                    <input type="email" placeholder="E-mail" value={email}
-                           onChange={e => setEmail(e.target.value)}/>
-                    <label>Password</label>
-                    <input type="password" placeholder="Password" value={password}
-                           onChange={e => setPassword(e.target.value)}/>
-                    <label>RG</label>
-                    <InputMask placeholder="RG" mask="99.999.999-9" value={rg}
-                               onChange={e => setRG(e.target.value.replace(/\D/g, ''))}/>
-                    <label>CPF</label>
-                    <InputMask placeholder="CPF" mask="999.999.999-99" value={cpf}
-                               onChange={e => setCpf(e.target.value.replace(/\D/g, ''))}/>
-                    <label>Birthday</label>
-                    <input type="date" value={birthDay} onChange={e => setBirthDay(e.target.value)}/>
-                    <label>Cellphone</label>
-                    <InputMask placeholder="Cellphone" mask="(99) 99999-9999" value={cellphone}
-                               onChange={e => setCellphone(e.target.value.replace(/\D/g, ''))}/>
-                    <button className="button" type="submit">
-                        {showLoading ? (
-                            <img className="loadingGif" src={loadingGif} alt="Spinner"/>
-                        ) : (
-                            userId === '0' ? 'Next' : 'Next'
-                        )}
-                    </button>
+                <div className="content">
+                    <section className="form">
+                        <img src={logoJP} alt="JP"/>
+                        <h1>{userId === '0' ? 'Add New' : 'Update'} User</h1>
+                        <p>Enter the user information and click on {userId === '0' ? 'Next' : 'Next'}</p>
+                        <Link className="back-link" to="/users">
+                            <div className="container-button">
+                                <div className="iconArrowLeft"><FiArrowLeft size={16} color="blue"/></div>
+                                <div className="textButton">Back to Users</div>
+                            </div>
+                        </Link>
+                    </section>
+                    <form onSubmit={SaveOrUpdateUser}>
+                        <label>Name*</label>
+                        <input id="name" placeholder="Name" value={name} onChange={e => setName(e.target.value)}/>
+                        <label>Email*</label>
+                        <input type="email" placeholder="E-mail" value={email}
+                               onChange={e => setEmail(e.target.value)}/>
+                        <label>Password*</label>
+                        <input type="password" placeholder="Password" value={password}
+                               onChange={e => setPassword(e.target.value)}/>
+                        <label>RG*</label>
+                        <InputMask placeholder="RG" mask="99.999.999-9" value={rg}
+                                   onChange={e => setRG(e.target.value.replace(/\D/g, ''))}/>
+                        <label>CPF*</label>
+                        <InputMask placeholder="CPF" mask="999.999.999-99" value={cpf}
+                                   onChange={e => setCpf(e.target.value.replace(/\D/g, ''))}/>
+                        <label>Gender*</label>
+                        <select onChange={e => setSexo(e.target.value)} value={sexo} className="select">
+                            <option value="Select" >Select</option>
+                            <option value="Male">Male</option>
+                            <option value="Female"> Female</option>
+                            <option value="Do Not Inform">Do not inform</option>
+                        </select>
+                        <label>Birthday*</label>
+                        <input type="date" value={birthDay} onChange={e => () => setBirthDay(e.target.value)}/>
+                        <label>Cellphone*</label>
+                        <InputMask placeholder="Cellphone" mask="(99) 99999-9999" value={cellphone}
+                                   onChange={e => setCellphone(e.target.value.replace(/\D/g, ''))}/>
+                        <button className="button" type="submit">
+                            {showLoading ? (
+                                <img className="loadingGif" src={loadingGif} alt="Spinner"/>
+                            ) : (
+                                userId === '0' ? 'Next' : 'Next'
+                            )}
+                        </button>
 
-                </form>
+                    </form>
+                </div>
             </div>
-        </div>
         </div>
     )
 }
