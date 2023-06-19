5999d783644642886fb5826766a9068b815fdb0d
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-06-19 11:16:48 -0300

    Changed Cors Origin

--- Backend/src/main/java/com/corporation/apiclient/config/FileStorageConfig.java
+++ Backend/src/main/java/com/corporation/apiclient/config/FileStorageConfig.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Getter
+@Getter 
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
---
+++ Frontend/front-client/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC64RpdiiJj5ZucDYqgW9DMCRnpahpnQ9I",
+    authDomain: "sistema-cliente.firebaseapp.com",
+    projectId: "sistema-cliente",
+    storageBucket: "sistema-cliente.appspot.com",
+    messagingSenderId: "140137848317",
+    appId: "1:140137848317:web:aab98373c3224ea645cd94"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
